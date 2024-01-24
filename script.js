6e7aadef7fc4eb51e12a7989b88581c7c336247c
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2024-01-24 16:43:43 +0530

    Commpleted

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-
-
 let speech = new SpeechSynthesisUtterance();
+let addVoices = [];
+let voiceSelect = document.querySelector("select");
 
-document.querySelector("button").addEventListener("click",()=>{
+window.speechSynthesis.onvoiceschanged = () => {
+    addVoices = window.speechSynthesis.getVoices();
+    speech.voice = addVoices[0];
+    addVoices.forEach((voice,i)=>(voiceSelect.options[i] = new Option(voice.name,i)))
+}
+
+voiceSelect.addEventListener("change",()=>{
+    speech.voice = addVoices[voiceSelect.value];
+})
+
+document.querySelector("button").addEventListener("click", () => {
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
     console.log("Button Clicked")
